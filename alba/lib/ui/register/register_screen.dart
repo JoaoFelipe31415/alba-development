e959fca63bda7bc93b6bbedd15d53df12ce956f1
João Felipe <joaofelipe343@hotmail.com>
2026-03-17 23:42:31 -0300

    refactor: consolidate register screen input fields into a new reusable InputRegister widget.

--- alba/lib/ui/register/register_screen.dart
+++ alba/lib/ui/register/register_screen.dart
@@ -1,3 +1,4 @@
+import 'package:alba/ui/design_system/widgets/input_register.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:alba/ui/design_system/constants/spaces.dart';
@@ -11,7 +12,7 @@ class RegisterScreen extends StatelessWidget {
     const Color textColor = Color(0xFF334155);
     const Color primaryColor = Color(0xFF1D4ED8);
     const Color buttonGreen = Color(0xFF84F41E);
-    
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -19,139 +20,92 @@ class RegisterScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: Spaces.xxl),
           child: SingleChildScrollView(
             child: Column(
+              spacing: Spaces.xxl,
               mainAxisSize: .max,
               mainAxisAlignment: .center,
               crossAxisAlignment: .start,
               children: [
-                  const SizedBox(height: Spaces.l),
+                const SizedBox(height: Spaces.l),
 
-                IconButton(onPressed: () {
-                  Navigator.pop(context);
-                }, 
-                icon: Image.asset(ImagesConstants.retornopage, 
-                width: Spaces.m, 
-                height: Spaces.xl
-                ),
-                ),
-          
-                const SizedBox(height: Spaces.xxl),
-                Text(
-                  'Qual é o seu e-mail?',
-                  style: TextStyle(
-                    color: textColor,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-
-                  ),
-                ),
-                const SizedBox(
-                  height: Spaces.m,
-                ),
-
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'E-mail',
-                    hintText: 'ex.: seu@email.com',
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(Spaces.s),
-                    ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Image.asset(
+                    ImagesConstants.retornopage,
+                    width: Spaces.m,
+                    height: Spaces.xl,
                   ),
                 ),
 
-                const SizedBox(height: Spaces.xxl),
-
-                Text('Digite uma Senha',
-                style: TextStyle(
-                  color: textColor,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                )
+                InputRegister(
+                  title: 'Qual é o seu e-mail?',
+                  labelText: 'E-mail',
+                  hintText: 'ex.: seu@email.com',
+                  obscureText: false,
                 ),
 
-                const SizedBox(height: Spaces.m),
-
-                TextField(
+                InputRegister(
+                  title: 'Digite uma Senha',
+                  labelText: 'Senha',
+                  hintText: '••••••••',
                   obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Senha',
-                    hintText: '••••••••',
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(Spaces.s),
-                    ),
-                  ),
                 ),
 
-                const SizedBox(height: Spaces.xxl),
-
-                Text('Confirme sua Senha',
-                style: TextStyle(
-                  color: textColor,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                )
-                ),
-
-                const SizedBox(height: Spaces.m),
-
-                TextField(
+                InputRegister(
+                  title: 'Confirme sua Senha',
+                  labelText: 'Confirmar Senha',
+                  hintText: '••••••••',
                   obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Confirmar Senha',
-                    hintText: '••••••••',
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(Spaces.s),
-                    ),
-                  )
                 ),
-
-                const SizedBox(height: Spaces.xxl),
 
                 ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(double.infinity, 50),
-                          backgroundColor: buttonGreen,
-                          shadowColor: textColor,
-                        ),
-                        child: Text('Cadastrar', style: TextStyle(color: primaryColor, 
-                        fontWeight: FontWeight.bold, fontSize: Spaces.xl, ), 
-                        ),
-                      ),
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 50),
+                    backgroundColor: buttonGreen,
+                    shadowColor: textColor,
+                  ),
+                  child: Text(
+                    'Cadastrar',
+                    style: TextStyle(
+                      color: primaryColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: Spaces.xl,
+                    ),
+                  ),
+                ),
 
                 const SizedBox(height: Spaces.l),
-          
+
                 Center(
                   child: Text.rich(
-                          TextSpan(
-                            text: 'Já possui uma conta? ',
-                            style: TextStyle(color: Colors.black),
-                            children: [
-                              TextSpan(
-                                text: 'Entrar',
-                                style: TextStyle(
-                                  color: primaryColor,
-                                  fontWeight: FontWeight.bold,
-                                  decoration: TextDecoration.underline,
-                                  decorationColor: primaryColor,
-                                ),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () {                     
-                                    //TODO: Implementar Whatsapp
-                                  },
-                              ),
-                            ],
+                    TextSpan(
+                      text: 'Já possui uma conta? ',
+                      style: TextStyle(color: Colors.black),
+                      children: [
+                        TextSpan(
+                          text: 'Entrar',
+                          style: TextStyle(
+                            color: primaryColor,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                            decorationColor: primaryColor,
                           ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              //TODO: Implementar Whatsapp
+                            },
                         ),
+                      ],
+                    ),
+                  ),
                 ),
-              ], 
+              ],
             ),
           ),
         ),
-      )
+      ),
     );
   }
 }
