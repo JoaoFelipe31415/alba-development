17f0eff188b221d3a3382819ff76e9d69a5d754c
YonaraMirelly <yonara.mirelly@ufrpe.br>
2026-06-13 21:29:59 -0300

    o usuário já viu/passou pelo onboarding neste dispositivo

--- alba/lib/main.dart
+++ alba/lib/main.dart
@@ -1,31 +1,57 @@
 import 'package:alba/config/dependencies.dart';
 import 'package:alba/core/locales.dart';
 import 'package:alba/ui/design_system/theme/app_colors.dart';
+import 'package:alba/ui/login/login_screen.dart';
 import 'package:alba/ui/onboarding/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'firebase_options.dart';
 
 const Color _albaLightBlue = Color(0xFF7FE2E1);
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   setupInjector();
-  runApp(const MainApp());
+
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  final bool onboardingVisto = prefs.getBool('onboarding_visto') ?? false;
+
+  runApp(
+    MainApp(
+      onboardingVisto: onboardingVisto,
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final bool onboardingVisto;
+
+  const MainApp({
+    super.key,
+    required this.onboardingVisto,
+  });
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      supportedLocales: const [Locale('pt', 'BR')],
+      supportedLocales: const [
+        Locale('pt', 'BR'),
+      ],
       debugShowCheckedModeBanner: false,
-      home: const OnboardingScreen(),
+
+      home: onboardingVisto
+          ? const LoginScreen()
+          : const OnboardingScreen(),
+
       theme: ThemeData(
         useMaterial3: true,
         brightness: Brightness.light,
@@ -89,4 +115,4 @@ class MainApp extends StatelessWidget {
       ],
     );
   }
-}
+} 
